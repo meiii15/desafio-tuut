d0ea6f83ae16a4e67c95f85c439c5972d7f39a4f
Meiii15 <pedroxsqueiroz@gmail.com>
2018-06-18 01:20:10 -0300

    Ajustando galeria

--- theme/parts/galery/galery.js
+++ theme/parts/galery/galery.js
@@ -1,25 +1,73 @@
 (function($){
-    $('#carousel').carousel({
-        interval: 10000
-    });
-      
+    var target = 'http://' +  window.location.hostname  + '/wp/wp-admin/admin-ajax.php' ;
     
-    $('.carousel .carousel-item').each(function(){
+    $.ajax({
+        url: target,
+        type:'get',
+        data: { 'action': 'get_images_galery'}
+    }).done(function(data){
         debugger;
-        
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-        
-        for (var i=0;i<2;i++) {
-            next=next.next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
+
+        var images = [];
+        for(let currrentImageUrlIndex in data){
+            let currentImageGalery = data[currrentImageUrlIndex];
             
-            next.children(':first-child').clone().appendTo($(this));
+            let imageGalery = $("<img>");
+            imageGalery.attr('src', currentImageGalery);
+            
+            imageGalery.click(function(e){
+                $("#imageGaleryModal").find('.modal-dialog').css('max-width', 'fit-content');
+                
+                var imageModal = $("#imageGaleryModal").find('img');
+                imageModal.attr('src', currentImageGalery);
+                $("#imageGaleryModal").modal();
+            });
+
+            images.push(imageGalery);
         }
+        
+        var carouselItens = [];
+        var isFirst = true;
+        const SHOWING_IMAGES = 3;
+
+        while(images.length > 0)
+        {
+            let carouselItem = $("<div>");
+            carouselItem.addClass("carousel-item");
+
+            if(isFirst){
+                carouselItem.addClass('active');
+                isFirst = false;
+            }
+
+            var imagesItemCount = images.length > SHOWING_IMAGES ? SHOWING_IMAGES : images.length;
+            var itemImages = images.splice(0, imagesItemCount);
+
+            carouselItem.append(itemImages);
+            carouselItens.push(carouselItem);
+        }
+
+        $(".carousel-inner").append(carouselItens);
+        $('#carousel').carousel({
+            interval: 10000
+        });
     });
+
+    
+    // $('.carousel .carousel-item').each(function(){
+    //     var next = $(this).next();
+    //     if (!next.length) {
+    //         next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+        
+    //     for (var i=0;i<2;i++) {
+    //         next=next.next();
+    //         if (!next.length) {
+    //             next = $(this).siblings(':first');
+    //         }
+            
+    //         next.children(':first-child').clone().appendTo($(this));
+    //     }
+    // });
 })(jQuery);
--- theme/parts/galery/galery.php
+++ theme/parts/galery/galery.php
@@ -8,57 +8,42 @@
 
 
 <section class="galery-section">
-    
     <h1>Galeria TOOT</h1>
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+        </div>
 
-    <div class="galery-carousel">
+        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
-    <?php
-    $query_images_args = array(
-        'post_type'      => 'attachment',
-        'post_mime_type' => 'image',
-        'post_status'    => 'inherit',
-        'posts_per_page' => - 1,
-    );
+    <div class="modal" id="imageGaleryModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-    $query_images = new WP_Query( $query_images_args );
-
-    ?>
-        <div id="carousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-    <?php
-    
-    $isFirst = true;
-    
-    foreach ( $query_images->posts as $image ) {
-        
-        $images[] = wp_get_attachment_url( $image->ID );
-                
-        foreach($images as $currentImage)
-        {    
-            ?>
-                <div class="carousel-item <?php if ($isFirst) { echo 'active'; $isFirst = false; } ?>">
-                    <img class="d-block w-100" src="<?php echo $currentImage?>"/>
-                </div>
-            <?php
-        }
-
-    }
-        ?>
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Zoom</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
-                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+            <!-- Modal body -->
+            <div class="modal-body">
+                <img src=""/>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
 
             </div>
-        <?php
-    ?>
-    
+        </div>
     </div>
 </section>
