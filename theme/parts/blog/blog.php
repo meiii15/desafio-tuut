a58e19f27606b3911897c623af24c9ba1b2b994a
Meiii15 <pedroxsqueiroz@gmail.com>
2018-06-18 01:23:10 -0300

    Centralizando mostragem de posts do blog

--- theme/parts/blog/blog.php
+++ theme/parts/blog/blog.php
@@ -6,38 +6,32 @@
 <section class="blog-section">
     <h1>Blog da TOOT</h1>
     
-    <div class="posts-container">
-        <?php
-            $recent_posts = wp_get_recent_posts();
+    <div class="posts-container-horizontal">
+        <div class="posts-container">
+            <?php
+                $recent_posts = wp_get_recent_posts();
 
-            $indexCount = 0;
-            
-            foreach( $recent_posts as $recent ) {
-                ?>
-                    <div class="post-line">
-                        <div class="post-container <?php if($indexCount % 2) echo "right"; else echo "left"; ?>">
-                            <h2>
-                                <?php echo $recent["post_title"]; ?>
-                            </h2>
-    
-                            <div>
-                                <?php echo $recent["post_content"]; ?>
+                $indexCount = 0;
+                
+                foreach( $recent_posts as $recent ) {
+                    ?>
+                        <div class="post-line">
+                            <div class="post-container <?php if($indexCount % 2) echo "right"; else echo "left"; ?>">
+                                <h2>
+                                    <?php echo $recent["post_title"]; ?>
+                                </h2>
+        
+                                <div>
+                                    <?php echo $recent["post_content"]; ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php
-                
-                $indexCount++;
-                // printf( '<li>
-                //             <a href="%1$s">%2$s</a>
-                //             <div> %3$s </div>
-                //         </li>',
-                //     $recent['ID'],
-                //     $recent['post_content'],
-                //     print_r(array_keys($recent))
-                // );
-            }
-        ?>
+                    <?php
+                    
+                    $indexCount++;
+                }
+            ?>
+        </div>
     </div>
 
 </section>
